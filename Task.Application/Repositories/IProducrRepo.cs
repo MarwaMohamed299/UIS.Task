6c93d24541433958e666eb7b21b9442bfcbee2b9
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2024-10-25 17:52:39 +0300

    Add project files.

---
+++ Task.Application/Repositories/IProducrRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Domain;
+
+namespace Task.Application.Repositories
+{
+    public interface IProducrRepo
+    {
+        Task AddProductAsync(Product product);
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+    }
+}
---
+++ Task.Domain/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Domain
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public decimal TotalPrice { get; set; }
+        public Product Product { get; set; } 
+    }
+}
